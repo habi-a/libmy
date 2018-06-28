cdfa8852de9564f5bc4041d9e8c54feb41bcffea
Açal Habi <habi_a@etna-alternance.net>
2018-06-28 18:02:37 +0200

    sort

--- inc/my.h
+++ inc/my.h
@@ -7,13 +7,12 @@ typedef enum e_bool
     true
 } t_bool;
 
-char		**malloc2d_char(unsigned int row, unsigned int column);
-void		free2d_char(char **array2d, unsigned int row);
 int		my_getnbr(const char *str);
 unsigned int	my_isalnum(char c);
 void		my_putchar(char c);
 void		my_putnbr(int n);
 void		my_putstr(const char *str);
+void my_sort_str_tab(char **tab, unsigned int size);
 char		*my_strcat(char *dest, const char *src);
 int		my_strcmp(const char *s1, const char *s2);
 char *my_strdup(const char *src);
