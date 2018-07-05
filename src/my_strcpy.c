9ae9580ed693858272645475de328f42def3ed97
Açal Habi <habi_a@etna-alternance.net>
2018-07-05 14:13:15 +0200

    add customize strdup strcpy

--- src/my_strcpy.c
+++ src/my_strcpy.c
@@ -11,3 +11,34 @@ char *my_strcpy(char *dest, const char *src)
     dest[i] = '\0';
     return (dest);
 }
+
+char *my_strncpy(char *dest, const char *src, unsigned int n)
+{
+    unsigned int i;
+
+    i = 0;
+    while (src[i] && i < n)
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+    return (dest);
+}
+
+char *my_strrcpy(char *dest, const char *src, unsigned int r)
+{
+    unsigned int i;
+    unsigned int j;
+
+    i = 0;
+    j = r;
+    while (src[j])
+    {
+        dest[i] = src[j];
+        i++;
+        j++;
+    }
+    dest[i] = '\0';
+    return (dest);
+}
--- src/my_strdup.c
+++ src/my_strdup.c
@@ -3,14 +3,37 @@
 
 char *my_strdup(const char *src)
 {
-    int len;
     char *dest;
+    unsigned int len;
 
     len = my_strlen(src) + 1;
-    dest = malloc((unsigned)len * sizeof(char));
+    dest = malloc(len * sizeof(char));
     if (dest == NULL)
         return (NULL);
-    dest = my_strcpy(dest, src);
-    dest[len - 1] = '\0';
+    my_strcpy(dest, src);
+    return (dest);
+}
+
+char	*my_strndup(char *str, unsigned int n)
+{
+    char *dest;
+
+    dest = malloc((n + 1) * sizeof(char));
+    if (dest == NULL)
+        return (NULL);
+    my_strncpy(dest, str, n);
+    return (dest);
+}
+
+char	*my_strrdup(char *str, unsigned int r)
+{
+    char *dest;
+    unsigned int len;
+
+    len = my_strlen(str) - r + 1;
+    dest = malloc(len * sizeof(char));
+    if (dest == NULL)
+        return (NULL);
+    my_strrcpy(dest, str, r);
     return (dest);
 }
